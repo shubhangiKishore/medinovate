f6b6df80c7603fa46c98a6199bb13f8c67dc31b5
shubhangiKishore <shubhangi.kishore96.com>
2017-10-29 14:30:57 +0530

    done

--- test.py
+++ test.py
@@ -1,20 +1,27 @@
 from flask import Flask
 from flask import request
+import requests
 from PIL import Image
 import imagehash
 import os
 from flask import render_template
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['STATIC_FOLDER'] = 'static'
 UPLOAD_FOLDER = '/home/sk/Documents/innovacer'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def upload_t():
-    file = request.files['imagefile']
+
+    prescription = request.form['prescription']
+    #diagnosis = request.form['diagnosis']
+    #p_key = request.form['p-key']
+    #file = request.files['imagefile']
     if file:
         file_name = "c"
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
-        return 'OK'
+        return prescription
 
 
 '''
@@ -29,16 +36,44 @@ def upload_file():
 
 @app.route('/')
 def index():
-    return render_template('display.html')
+    return render_template('medinnovate/index.html')
 
 
-@app.route('/hello', methods = ['GET','POST'])
+
+@app.route('/add-patient')
+
+
+def add_pat():
+    return render_template('medinnovate/add-patient.html')
+
+
+@app.route('/submit-record', methods = ['GET','POST'])
 def hello():
-    if request.method == 'POST':
-        upload_t()
+    #if request.method == 'POST':
+    #    upload_t()
 
-    hash = imagehash.average_hash(Image.open("c"))
-    return str(hash)
+    prescription = request.form['prescription']
+    diagnosis = request.form['diagnosis']
+    id  = request.form['p-key']
+    accessible_by = request.form['p-key']
+    image_file = request.files['imagefile']
+    record_type = 'xray'
+    datacenter = 'bangalore'
+
+
+    payload = {'id': id, 'record_type': 'xray', 'datacenter': 'bangalore', 'image_file': image_file, 'accessible_by': id}
+
+    #r = requests.post("http://httpbin.org/post", data=payload)
+
+
+
+    url = "http://52.91.199.207:82/addRecord"
+    response = requests.request("POST", url, data=payload)
+
+
+    print (response)
+    return "Details added!"
+
 
 
 
